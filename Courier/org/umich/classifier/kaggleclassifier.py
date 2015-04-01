4c98d3a49d93d24be57ba52cffe190faa9c5624c
Jason Xie <jasonx@umich.edu>
2015-04-01 19:06:57 -0400

    fixed kaggleclassifer.py to work with our dataset STILL NEED TO TEST

--- Courier/org/umich/classifier/kaggleclassifier.py
+++ Courier/org/umich/classifier/kaggleclassifier.py
@@ -5,6 +5,8 @@ uniquename: nbhutani
 '''
 from collections import defaultdict
 from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import StratifiedKFold
+from sklearn import metrics
 import sys
 import os
 import tokenizer
@@ -19,24 +21,42 @@ non_punctuation_count_feature = 'nonpunctuationcount'
 doc_length_feature = 'doclength'
 pronoun_count_feature = 'pronouncount'
 
-def main(data_dir):
-    features, classifier = train()
-    test(data_dir, classifier, features)
+def main(test_dir, train_dir, label_file):
+    filenames, y = read_data(train_dir, label_file)
     
-def train():
+    skf = StratifiedKFold(y, n_folds=5)
+    # split data into 5 sets of train/test data
+    for train_index, test_index in skf:
+        # filenames to train on
+        f_train = filenames[train_index]
+        y_train = y[train_index]
+
+        f_test = filenames[test_index]
+        y_test = y[test_index]
+        
+        features, classifier = train(train_dir, f_train, y_train)
+        test(train_dir, f_test, y_test, classifier, features)
+
+def read_data(dirname, label_file):
+    _, _, files = os.walk(train_dir)
+    filenames = [file for file in sublist for sublist in files]
+
+    labels = np.genfromtxt(fname=label_file, skip_header=1, delimiter=',', usecols=(1), converters={1:lambda s: 1 if s == 1 else -1})
+
+    return filenames, labels
+    
+def train(dirname, f_train, y_train):
     global my_tokenizer, mix_label, joke_label
-    train_dir = 'kaggle.training/'
-    train_files = os.listdir(train_dir)
     inverted_index = defaultdict(dict)
-    for train_file in train_files:
-        infile = open(train_dir + train_file)
-        docId = getDocId(train_file)
-        doc_content = infile.read()
-        infile.close()
-        indexDocument(doc_content, docId, inverted_index)
+    for train_file in f_train:
+        with open(dirname+train_file) as f:
+            docId = getDocId(train_file)
+            doc_content = f.read()
+            indexDocument(doc_content, docId, inverted_index)
     features = getFeatures(inverted_index)
     designMatrix = getDesignMatrix(inverted_index, features, train_files)
-     
+
+    """
     train_labels = numpy.zeros(len(train_files)) 
     train_label_file = 'solution.train.csv'
     infile = open(train_label_file)
@@ -46,39 +66,43 @@ def train():
         doc_id = getDocId(row[0])
         label = joke_label if row[1].strip() == 'joke' else mix_label
         train_labels[doc_id - 1] = label
-    infile.close()    
+    infile.close()
+    """
     classifier = LogisticRegression()
-    classifier.fit(designMatrix, train_labels)
+    classifier.fit(designMatrix, y_train)
     return features, classifier
 
-def test(data_dir, classifier, features):
+def test(dirname, f_test, y_test, classifier, features):
     global my_tokenizer, mix_label, joke_label
-    test_files = os.listdir(data_dir)
-    output = open('test_results_lr.csv', 'w')
-    output.write('File,Class\n')
-    print('File,Class')
-    for test_file in test_files:
-        infile = open(data_dir + test_file)
-        doc_content = infile.read()
-        text_length, punctuation_count, non_punctuation_count,  = my_tokenizer.getFeatures(doc_content)
-        tokens = my_tokenizer.getTokens(doc_content)
-        pronoun_count = my_tokenizer.getPronounCount(tokens)
-        feature = numpy.zeros(len(features))
-        for token in set(tokens):
-            if token in features:
-                token_index = features.index(token)
-                term_count = tokens.count(token)
-                feature[token_index] = term_count
-        feature[features.index(punctuation_count_feature)] = punctuation_count
-        feature[features.index(non_punctuation_count_feature)] = non_punctuation_count
-        feature[features.index(doc_length_feature)] = text_length
-        feature[features.index(pronoun_count_feature)] = pronoun_count
-        predicted_label = int(classifier.predict(feature)[0])
-        predicted_class = 'joke' if predicted_label == joke_label else 'mix'
-        row = test_file + ',' + str(predicted_class) 
-        print row       
-        output.write("%s\n" % row)
-    output.close    
+#    output = open('test_results_lr.csv', 'w')
+#    output.write('File,Class\n')
+#    print('File,Class')
+    y_predicted = []
+    for test_file in f_test:
+        with open(dirname+test_file) as f:
+            doc_content = f.read()
+            text_length, punctuation_count, non_punctuation_count,  = my_tokenizer.getFeatures(doc_content)
+            tokens = my_tokenizer.getTokens(doc_content)
+            pronoun_count = my_tokenizer.getPronounCount(tokens)
+            feature = numpy.zeros(len(features))
+            for token in set(tokens):
+                if token in features:
+                    token_index = features.index(token)
+                    term_count = tokens.count(token)
+                    feature[token_index] = term_count
+            feature[features.index(punctuation_count_feature)] = punctuation_count
+            feature[features.index(non_punctuation_count_feature)] = non_punctuation_count
+            feature[features.index(doc_length_feature)] = text_length
+            feature[features.index(pronoun_count_feature)] = pronoun_count
+            y_predicted.append(int(classifier.predict(feature)[0]))
+
+    # calc and print accuracy score of the prediction
+    print metrics.accuracy_score(y_test, y_predicted)
+#            predicted_class = 'joke' if predicted_label == joke_label else 'mix'
+#            row = test_file + ',' + str(predicted_class) 
+#            print row       
+#            output.write("%s\n" % row)
+#    output.close    
 
 def getDocId(doc_file_name):    
     return int(re.sub('^[a-z]+.', '', doc_file_name).lstrip('0'))
@@ -112,4 +136,4 @@ def indexDocument(document_content, doc_id, inverted_index):
     return inverted_index
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
