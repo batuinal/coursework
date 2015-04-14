9559846ff31cc53ceae623f1eb3098fd9ff4f133
nbhutani <nikibhutani18@gmail.com>
2015-04-14 16:02:39 -0400

    Resolving conflicts

--- Courier/org/umich/classifier/classifier.py
+++ Courier/org/umich/classifier/classifier.py
@@ -20,7 +20,6 @@ def main(test_dir, train_dir, test_labels, train_labels):
     test_files, y_test = read_data(test_dir, test_labels)
 
     features, classifier = train(train_files, y_train)
-    print 'feature count: ', len(features)
     test(test_files, y_test, classifier, features)
     """
     skf = StratifiedKFold(y, n_folds=5)
@@ -30,7 +29,7 @@ def main(test_dir, train_dir, test_labels, train_labels):
         # filenames to train on
         f_train = filenames[train_index]
         y_train = y[train_index]
-        
+
         f_test = filenames[test_index]
         y_test = y[test_index]
 
@@ -39,7 +38,7 @@ def main(test_dir, train_dir, test_labels, train_labels):
         print 'Testing fold'
         test(f_test, y_test, classifier, features)
     """
-    
+
 def read_data(dirname, label_file):
     filenames = []
     for path, _, files in os.walk(dirname):
@@ -47,7 +46,7 @@ def read_data(dirname, label_file):
     filenames = sorted(filenames)
     labels = numpy.genfromtxt(fname=label_file, skip_header=1, delimiter=',', usecols=(1), converters={1:lambda s: 1 if s == '1' else -1})
     return numpy.array(filenames), labels
-    
+
 def train(documents, labels):
     print 'Training'
     global my_tokenizer
@@ -61,38 +60,35 @@ def train(documents, labels):
 def test(documents, labels, classifier, features):
     print 'Testing'
     global my_tokenizer
-    inverted_index = indexDocuments(documents)     
+    inverted_index = indexDocuments(documents)
     design_matrix = getDesignMatrix(inverted_index, features, documents)
-    # calc and print accuracy score of the prediction
     predicted = classifier.predict(design_matrix)
     f1_score = metrics.f1_score(labels, predicted, average='micro')
     accuracy = metrics.accuracy_score(labels, predicted)
-    print predicted
     print accuracy, f1_score
     return accuracy, f1_score
-    #print classifier.score(design_matrix, labels)   
 
 def getDocId(doc_file_name):
     mid = re.sub('^[A-Za-z\-]+', '', os.path.basename(doc_file_name)).lstrip('0')
     return int(re.sub('.html$', '', mid))
- 
+
 def getFeatures(inverted_index):
     features_list = []
     threshold = 10
     for term in inverted_index.keys():
         document_frequency = len(inverted_index[term])
         if document_frequency > threshold:
-            features_list.append(term) 
-    return features_list 
- 
-def getDesignMatrix(inverted_index, features, documents): 
+            features_list.append(term)
+    return features_list
+
+def getDesignMatrix(inverted_index, features, documents):
     design_matrix = numpy.zeros((len(documents), len(features)))
     docids = [getDocId(d) for d in documents]
     for termIndex in range(len(features)):
         term = features[termIndex]
         docs = inverted_index[term]
         for doc in docs:
-            design_matrix[docids.index(doc)][termIndex] = inverted_index[term][doc] 
+            design_matrix[docids.index(doc)][termIndex] = inverted_index[term][doc]
     return design_matrix
 
 def indexDocuments(documents):
@@ -101,10 +97,10 @@ def indexDocuments(documents):
         with open(data_file) as f:
             docId = getDocId(data_file)
             doc_content = f.read()
-            indexDocument(doc_content, docId, inverted_index)  
-    return inverted_index       
+            indexDocument(doc_content, docId, inverted_index)
+    return inverted_index
 
-def indexDocumentForTag(document_content, doc_id, inverted_index): 
+def indexDocumentForTag(document_content, doc_id, inverted_index):
     global my_tokenizer
     terms = my_tokenizer.getTokensForTag(document_content, 'title')
     vocab_terms = set(terms)
@@ -113,8 +109,8 @@ def indexDocumentForTag(document_content, doc_id, inverted_index):
         inverted_index[term][doc_id] = term_frequency
     return inverted_index
 
-#indexes the document, updates dictionary        
-def indexDocument(document_content, doc_id, inverted_index):  
+#indexes the document, updates dictionary
+def indexDocument(document_content, doc_id, inverted_index):
     global my_tokenizer
     metadata = my_tokenizer.getMetaData(document_content)
     terms = my_tokenizer.getTokens(document_content)
@@ -122,7 +118,7 @@ def indexDocument(document_content, doc_id, inverted_index):
     for term in vocab_terms:
         term_frequency = terms.count(term)
         inverted_index[term][doc_id] = term_frequency
-    for entry in metadata.keys(): 
+    for entry in metadata.keys():
         inverted_index[entry][doc_id] = metadata[entry]
     return inverted_index
 
